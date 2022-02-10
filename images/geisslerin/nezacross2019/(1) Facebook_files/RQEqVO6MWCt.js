b12c69dac0ab8cc18b8c7b661d297908b88483de
dzulum <dzulum@mortemale.org>
2022-02-10 15:19:31 -0600

    geisslerin fotos ok

--- images/geisslerin/nezacross2019/(1) Facebook_files/RQEqVO6MWCt.js
+++
@@ -1,3 +0,0 @@
-if (self.CavalryLogger) { CavalryLogger.start_js_script(document.currentScript); }
-
-__d("LSUpsertGradientColor",[],(function(a,b,c,d,e,f){function a(){var a=arguments,b=a[a.length-1];b.n;var c=[];return b.seq([function(c){return b.db.table(117).put({a:a[0],b:a[1],c:a[2],d:[0,0]})},function(a){return b.resolve(c)}])}e.exports=a}),null);
